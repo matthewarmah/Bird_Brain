6a0f048e0ce95c37b51656a0bbd26639fd5be11d
matthew armah <matthewarmah@matthews-mbp.attlocal.net>
2021-11-11 17:31:25 -0800

    modified lesson 1 2 and added lesson 3

--- Lesson_2.py
+++ Lesson_2.py
@@ -1,5 +1,5 @@
-from BirdBrain import Finch
-from time import sleep
+from BirdBrain import Finch      #use the finch class that is located in the BirdBrain.py module/library 
+from time import sleep               #use the sleep class that is located in the time module
 bird = Finch()
 
 print("exercise 1")
@@ -11,3 +11,8 @@ print("excercise 2")
 bird.getLight("R")
 
 print("light",bird.getLight("R"))
+
+print("exercise 3")
+print(type(bird.getDistance()))
+
+print("exercise 4")
---
+++ Lesson_3.py
@@ -0,0 +1,10 @@
+from BirdBrain import Finch
+from time import sleep
+
+print("exercise 1")
+bird=finch()
+bird.setBeak(100,0,0)
+sleep(1)
+bird.setBeak(0,0,0)
+
+print("exercise 2")
